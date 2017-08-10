fbe2366d3eef1aefad26d925753d83f08c987af3
Vincent Melia <vincentmelia@Vincents-MacBook-Pro.local>
2017-08-09 21:10:15 -0400

    root url env var

--- Configuration.py
+++ Configuration.py
@@ -2,7 +2,7 @@ import os
 
 BasketballPlayerDatabase = 'BasketballPlayerDatabase.p'
 
-Root_URL = 'https://' + os.getenv('HEROKU_APP_NAME') + '.herokuapp.com'
+Root_URL = 'https://' + os.getenv('basketball_root_url')
 #Root_URL = '/'
 #Root_URL = 'https://joesbasketball3.herokuapp.com/'
 #Root_URL = 'https://joesbasketballtest.herokuapp.com/'
