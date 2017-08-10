acda7a609f455d4f8adf7aaee281ec491185701d
Vincent Melia <vincentmelia@Vincents-MacBook-Pro.local>
2017-08-09 20:24:18 -0400

    fix root url

--- Configuration.py
+++ Configuration.py
@@ -1,5 +1,6 @@
 BasketballPlayerDatabase = 'BasketballPlayerDatabase.p'
 
-Root_URL = '/'
+#Root_URL = '/'
 #Root_URL = 'https://joesbasketball3.herokuapp.com/'
 #Root_URL = 'https://joesbasketballtest.herokuapp.com/'
+Root_URL = 'https://pure-refuge-18946.herokuapp.com'
