0be04845f3495559c517f6085d7cde0e2ada309c
Vincent Melia <vincentmelia@Vincents-MacBook-Pro.local>
2017-08-09 20:59:07 -0400

    make root URL dynamic

--- Configuration.py
+++ Configuration.py
@@ -1,8 +1,11 @@
+import os
+
 BasketballPlayerDatabase = 'BasketballPlayerDatabase.p'
 
+Root_URL = 'https://' + os.getenv('HEROKU_APP_NAME') + '.herokuapp.com'
 #Root_URL = '/'
 #Root_URL = 'https://joesbasketball3.herokuapp.com/'
 #Root_URL = 'https://joesbasketballtest.herokuapp.com/'
-Root_URL = 'https://pure-refuge-18946.herokuapp.com'
+#Root_URL = 'https://pure-refuge-18946.herokuapp.com'
 
 #also have to change env in main file
