b3785c6238304e0bcc252b308680e7e4e1c133dd
Vincent Melia <vincentmelia@Vincents-MBP.example.com>
2017-08-12 13:38:48 -0400

    delete comments

--- Configuration.py
+++ Configuration.py
@@ -3,9 +3,3 @@ import os
 BasketballPlayerDatabase = 'BasketballPlayerDatabase.p'
 
 Root_URL = 'https://' + os.getenv('basketball_root_url')
-#Root_URL = '/'
-#Root_URL = 'https://joesbasketball3.herokuapp.com/'
-#Root_URL = 'https://joesbasketballtest.herokuapp.com/'
-#Root_URL = 'https://pure-refuge-18946.herokuapp.com'
-
-#also have to change env in main file
